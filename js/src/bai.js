1c6493a6509b677109c09b4dc3221b5c6bb8bd14
xiaopeng <1281876368@qq.com>
2018-12-22 21:56:25 +0800

    Site updated: 2018-12-22 21:56:19

---
+++ js/src/bai.js
@@ -0,0 +1,7 @@
+;(function(){
+    var bp = document.createElement('script');
+    bp.src = '//push.zhanzhang.baidu.com/push.js';
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+
